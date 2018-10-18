c575b2799915b018baa70e89e7d5ce985106abae
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-18 22:40:44 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School 
+  
+  attr_accessor :roster
+  
+  def initialize(school)
+    self.roster = {}
+  end
+  
+  def add_student(name, grade)
+    if self.roster.keys.include?(grade)
+      self.roster[grade] << name
+    else 
+      self.roster[grade] = [] 
+      self.roster[grade] << name 
+    end 
+  end 
+  
+  def grade(grade)
+    self.roster[grade]
+  end 
+  
+  def sort
+    
+    self.roster.sort 
+    self.roster
+  end  
+end 
